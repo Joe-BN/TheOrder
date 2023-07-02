7d6ec839d594b5eef095dca08c573f2955eb28b3
JBN <128038111+Joe-BN@users.noreply.github.com>
2023-07-02 22:55:04 +0300

    Add files via upload

---
+++ simpleorganiser.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+path = input("Enter Folderpath here ")
+files = os.listdir (path)
+
+for file in files:
+    filename,extension = os.path.splitext(file)
+    extension = extension[1:]
+    if os.path.exists(path+'/'+extension):
+        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
+    else:
+        os.makedirs(path+'/'+extension)
+        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
